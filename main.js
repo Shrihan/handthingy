f3339326646b7430a267f0a273c8fde40a081b60
Shrihan <114194629+Shrihan@users.noreply.github.com>
2023-12-03 19:35:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+prediction_1=""
+prediction_2=""
+Webcam.set({
+    width:300,
+    height:299,
+    image_format:"png",
+    png_quality:90
+})
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+function takesnapshot(){
+Webcam.snap(function(data_uri){
+    document.getElementById("selfieimage").src=data_uri
+})
+}
+console.log("ml5 version", ml5.version)
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/0DFLH8IId/model.json",modelLoaded)
+function modelLoaded(){
+    console.log("Model Loaded!")
+}
+function speak(){
+    var synth = window.speechSynthesis.Synhesis;
+    speak_data_1 = "The first prediction is" + prediction_1;
+    speak_data_2 =  "And the second prediction is" + prediction_2;
+    var utterThis=new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
